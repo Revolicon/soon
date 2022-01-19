46b7b26487a05d9b07da63052d385f2c3b88a636
Berk Altıok <berkpw@gmail.com>
2022-01-19 22:40:33 +0300

    chore: move components

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,32 @@
 import '../styles/globals.scss'
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>Revolicon - Beautifully Crafted Open Source Icons</title>
+        <meta name="title" content="Revolicon - Beautifully Crafted Open Source Icons" />
+        <meta name="description" content="Revolicon is an open-source icon library. It is designed for consistency and accessibility. The icons are carefully crafted for you to work seamlessly." />
+        <meta name="keywords" content="Revolicon, Font Icon, Web Icon, Icons, Icon, SVG Icon, SVG, Font, Revolution" />
+
+        <meta property="og:site_name" content="Revolicon" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://revolicon.com/" />
+        <meta property="og:title" content="Revolicon - Beautifully Crafted Open Source Icons" />
+        <meta property="og:description" content="Revolicon is an open-source icon library. It is designed for consistency and accessibility. The icons are carefully crafted for you to work seamlessly." />
+        <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://revolicon.com/" />
+        <meta property="twitter:title" content="Revolicon - Beautifully Crafted Open Source Icons" />
+        <meta property="twitter:description" content="Revolicon is an open-source icon library. It is designed for consistency and accessibility. The icons are carefully crafted for you to work seamlessly." />
+        <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
-export default MyApp
+export default App
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,77 @@
 import Head from 'next/head'
 import Image from 'next/image'
 
+import style from '../styles/home.module.scss'
+
+function Content() {
+  return (
+    <div className="flex flex-col items-center gap-4 text-center">
+      <img className="select-none" src="/logo.svg" alt="Logo" />
+      <div className="text-white text-opacity-72 text-lg leading-6 mb-2">Beautifully crafted open source icons.</div>
+      <div className="flex gap-3">
+        <a href="https://twitter.com/revolicon" className="inline-flex items-center justify-center px-6 py-2.5 bg-white rounded-full text-base font-medium leading-6 select-none">Follow us</a>
+      </div>
+    </div>
+  )
+}
+
+function Waitlist() {
+  return (
+    <div className={style.waitlist}>
+      <div className="flex flex-col text-base text-white">
+        <div className="font-medium">Join the waitlist</div>
+        <div className="text-white text-opacity-50">Sign up to be one of the first to use Revolicon.</div>
+      </div>
+      <form className="flex flex-col gap-3 sm:flex-row">
+        <input
+          type="email"
+          className="h-13 flex rounded-xl border border-white border-opacity-8 flex-1 bg-white/[.05] outline-none px-5 placeholder-white/[.5] text-white transition-colors duration-300 focus:outline-none"
+          placeholder="Email adress"
+        />
+        <button className="h-13 px-6 flex items-center justify-center border border-transparent text-white backdrop-blur-26 rounded-xl transition-all duration-300 hover:(border-white/[.08] bg-white/[.03])">Submit</button>
+      </form>
+    </div>
+  )
+}
+
+function Creators() {
+  return (
+    <div className="flex items-center gap-3">
+      <CreatorsItem link="https://twitter.com/emrhnugrl" badge="Designer" name="Emirhan Ugurlu"/>
+      <span className="text-white text-opacity-25">✦</span>
+      <CreatorsItem link="https://twitter.com/berkpw" badge="Developer" name="Berk Altıok"/>
+    </div>
+  )
+}
+function CreatorsItem({ name, link, badge }) {
+  return (
+    <div className="inline-block">
+      <span className="text-white text-opacity-25">{badge} </span>
+      <a
+        className="text-white text-opacity-50 hover:text-opacity-100 transition-colors duration-200"
+        href={link}
+        target="_blank"
+        rel="noreferrer"
+      >{name}</a>
+    </div>
+  )
+}
+
 export default function Home() {
   return (
-    <div>x</div>
+    <div className="w-full h-full flex justify-center items-center overflow-hidden px-4 relative overflow-hidden">
+      <div className="circle select-none pointer-events-none">
+        <div className="circle__inner">
+          <img src="/circle.png" alt="Circle"/>
+        </div>
+      </div>
+      <div className="max-w-[34rem] w-full flex flex-col items-center gap-[72px]">
+        <Content/>
+        <div className="flex flex-col items-center gap-4 w-full">
+          <Waitlist/>
+          <Creators/>
+        </div>
+      </div>
+    </div>
   )
 }
