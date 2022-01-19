b77e4695cad528d9f356c4a009989af16bd7b835
Berk Altıok <berkpw@gmail.com>
2022-01-19 20:29:25 +0300

    chore: move nextjs

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div>x</div>
+  )
+}
--- windi.config.js
+++
@@ -1,5 +0,0 @@
-import { defineConfig } from '@windicss/plugin-utils'
-
-export default defineConfig({
-  attributify: true
-})
