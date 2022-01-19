4f15d658f0e223d26e099163628496bbdf16fd5d
Berk Altıok <berkpw@gmail.com>
2022-01-19 23:40:18 +0300

    chore: update mail section

--- pages/index.js
+++ pages/index.js
@@ -25,10 +25,14 @@ function Waitlist() {
       <form className="flex flex-col gap-3 sm:flex-row">
         <input
           type="email"
-          className="h-13 flex rounded-xl border border-white border-opacity-8 flex-1 bg-white/[.05] outline-none px-5 placeholder-white/[.5] text-white transition-colors duration-300 focus:outline-none"
+          className="flex-1 text-white placeholder:text-white/[.5] rounded-xl leading-6 text-sm py-3.5 px-6 ring-1 ring-white ring-opacity-10 ring-inset bg-white bg-opacity-5 outline-none focus:outline-none transition-all duration-300 hover:ring-opacity-25 focus:ring-opacity-25"
           placeholder="Email adress"
         />
-        <button className="h-13 px-6 flex items-center justify-center border border-transparent text-white backdrop-blur-26 rounded-xl transition-all duration-300 hover:(border-white/[.08] bg-white/[.03])">Submit</button>
+        <button className="h-13 w-[52px] flex items-center justify-center ring-1 ring-white ring-opacity-10 ring-inset rounded-xl transition-all duration-300 bg-white bg-opacity-5 hover:ring-opacity-25">
+          <svg className="w-4 h-4" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M10.6655 2.62836C10.8707 2.44363 11.1869 2.46027 11.3716 2.66552L15.8716 7.66552C16.0427 7.85567 16.0427 8.14433 15.8716 8.33449L11.3716 13.3345C11.1869 13.5397 10.8707 13.5564 10.6655 13.3717C10.4602 13.1869 10.4436 12.8708 10.6283 12.6655L14.3773 8.5H0.499756C0.223613 8.5 -0.000244141 8.27615 -0.000244141 8C-0.000244141 7.72386 0.223613 7.5 0.499756 7.5H14.3773L10.6283 3.33449C10.4436 3.12923 10.4602 2.81309 10.6655 2.62836Z" fill="white"/>
+          </svg>
+        </button>
       </form>
     </div>
   )
