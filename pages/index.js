b7941415591fff8bf71d9551c917952827a286ac
Berk Altıok <berkpw@gmail.com>
2022-01-20 13:32:58 +0300

    feat: add google recaptcha

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 
 import style from '../styles/home.module.scss'
+import Waitlist from "../components/waitlist";
 
 function Content() {
   return (
@@ -70,29 +71,6 @@ function ContentSocial() {
           />
         </a>
       ))}
-    </div>
-  )
-}
-
-function Waitlist() {
-  return (
-    <div
-      className={style.waitlist}
-      data-aos="fade-up"
-      data-aos-delay={1400}
-    >
-      <div className="flex flex-col text-base text-white">
-        <div className="font-medium">Join the waitlist</div>
-        <div className="text-white text-opacity-50">Sign up to be one of the first to use Revolicon.</div>
-      </div>
-      <label className={style.waitlist__form}>
-        <input type="email" placeholder="Email adress" />
-        <button>
-          <svg className="w-4 h-4" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M10.6655 2.62836C10.8707 2.44363 11.1869 2.46027 11.3716 2.66552L15.8716 7.66552C16.0427 7.85567 16.0427 8.14433 15.8716 8.33449L11.3716 13.3345C11.1869 13.5397 10.8707 13.5564 10.6655 13.3717C10.4602 13.1869 10.4436 12.8708 10.6283 12.6655L14.3773 8.5H0.499756C0.223613 8.5 -0.000244141 8.27615 -0.000244141 8C-0.000244141 7.72386 0.223613 7.5 0.499756 7.5H14.3773L10.6283 3.33449C10.4436 3.12923 10.4602 2.81309 10.6655 2.62836Z" fill="white"/>
-          </svg>
-        </button>
-      </label>
     </div>
   )
 }
