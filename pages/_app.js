f774498d55db86bee3ace3a06cf2ad0af7ff0f6d
Berk Altıok <berkpw@gmail.com>
2022-01-20 04:09:58 +0300

    chore: animations optimized

--- pages/_app.js
+++ pages/_app.js
@@ -32,13 +32,13 @@ function App({ Component, pageProps }) {
         <meta property="og:url" content="https://revolicon.com/" />
         <meta property="og:title" content="Revolicon - Beautifully Crafted Open Source Icons" />
         <meta property="og:description" content="Revolicon is an open-source icon library. It is designed for consistency and accessibility. The icons are carefully crafted for you to work seamlessly." />
-        <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+        <meta property="og:image" content="#" />
 
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content="https://revolicon.com/" />
         <meta property="twitter:title" content="Revolicon - Beautifully Crafted Open Source Icons" />
         <meta property="twitter:description" content="Revolicon is an open-source icon library. It is designed for consistency and accessibility. The icons are carefully crafted for you to work seamlessly." />
-        <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+        <meta property="twitter:image" content="#" />
       </Head>
       <Component {...pageProps} />
     </>
